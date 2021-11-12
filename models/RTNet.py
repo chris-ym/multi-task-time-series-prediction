14a289efbf4b838971b74c29e5db98e61692abae
chris-ym <54630743+chris-ym@users.noreply.github.com>
2021-11-12 16:14:37 +0800

    Create RTNet.py

---
+++ models/RTNet.py
@@ -0,0 +1,22 @@
+#############
+import tensorflow as tf
+import numpy as np
+import math
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../utils'))
+sys.path.append(os.path.join(BASE_DIR, '../../utils'))
+import transformer_encoder
+import utils
+
+#############
+if __name__=='__main__':
+    batch_size = 64
+    d_k = 64
+    d_v = 64
+    ff_dim = 256
+    seq_len=8
+    n_heads=2
+    look_ahead_mask = transformer_encoder.create_look_ahead_mask(seq_len)
