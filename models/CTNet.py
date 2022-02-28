4ccfc12f01f2ad31d50ac4a9e99f283b322c68b6
chris-ym <54630743+chris-ym@users.noreply.github.com>
2022-02-28 18:08:35 +0800

    Update CTNet.py

--- models/CTNet.py
+++ models/CTNet.py
@@ -51,7 +51,11 @@ def create_model(data_df, output_bias=None):
     else:
       out2 = Dense(1, activation='sigmoid',name='binary_class')(x)        
       
-    return out1, out2
+    model = Model(inputs=in_seq, outputs=[out1,out2])
+
+    #model.compile(loss=['mse','binary_crossentropy'], optimizer=adam,metrics=['acc'],loss_weights=[ 1., 100.])
+
+    return model 
 
 #############
 if __name__=='__main__':
