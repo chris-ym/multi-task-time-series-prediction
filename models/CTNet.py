15efce8cddb1b12d77bc330ee47f94c8314577d8
chris-ym <54630743+chris-ym@users.noreply.github.com>
2022-03-04 15:01:21 +0800

    Update CTNet.py

--- models/CTNet.py
+++ models/CTNet.py
@@ -21,7 +21,7 @@ from keras.layers import Input, Dropout, Dense, LSTM, TimeDistributed, RepeatVec
 #features=np.array(total_X_train_cb[0]).shape[2]
 #################################################################################################
 
-def create_model(data_df, num_trans_enc, output_bias=None):
+def create_model(data_df, num_trans_enc, d_k=64, d_v=64, ff_dim=256, n_heads=2, output_bias=None):
     
     batch_size = data_df.shape[0]
     time_steps = data_df.shape[1] 
